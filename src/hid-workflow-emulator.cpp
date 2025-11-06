24821fcbb1e4a2c78f1427c5b2fdc4e561eadae3
awesomebrownies <evan.n.brown25@gmail.com>
2025-11-05 18:06:13 -0700

    Add programming workflow

--- src/hid-workflow-emulator.cpp
+++ src/hid-workflow-emulator.cpp
@@ -4,6 +4,15 @@
 USBKeyboard key;
 
 /*
+ * Check for local commits that need to be pushed
+ * Check origin for commit pulls
+ * Check for code that has not been committed
+ */
+void githubAnalyzation()
+{
+}
+
+/*
  * Loops through each character and types it
  */
 void typeAnimation(String s)
@@ -27,13 +36,21 @@ void openApplication(String s)
     key.key_code_raw(0x28); // enter
 }
 
-String query(String prompt)
+/*
+ * Ctrl + B - Opens bookmark menu
+ */
+void openBookmarkFolder(int bookmarkItem)
+{
+    // do this later!
+}
+
+String query(const char *prompt)
 {
     key.key_code('t', 0x05); // terminal shortcut
     delay(750);
-    // key.printf("chmod 666 /dev/ttyACM0\n"); // allows communication to usb
-    delay(5000);
-    key.printf("bash -c 'echo Reading input for next 5 seconds...; read r; echo $r > /dev/ttyACM0'\n"); // script to collect user feedback
+    key.printf("bash -c 'clear;echo ");
+    key.printf(prompt);
+    key.printf("; read r; echo $r > /dev/ttyACM*'\n"); // script to collect user feedback
 
     delay(1000);
 
@@ -53,29 +70,11 @@ String query(String prompt)
         }
     }
 
-    delay(500);
+    delay(200);
+    key.printf("exit\n"); // close terminal window
+    delay(200);
 
-    typeAnimation("Logged: " + response);
-}
-
-/*
- * Retrieve stored workflow state (store via enum)
- */
-void setup()
-{
-    Serial.begin(9600);
-    // method testing code
-    delay(1000);
-    query("blank");
-}
-
-/*
- * Check if on Linux: change boot order
- * Check if on Windows: change boot order
- * Restart machine
- */
-void dualBootWorkflow()
-{
+    return response;
 }
 
 /*
@@ -92,6 +91,41 @@ void dualBootWorkflow()
  */
 void programmingWorkflow()
 {
+    String response = query("Enter subworkflow? [minecraft], [research], [other] ... 5s");
+
+    openApplication("firefox");
+    if (response.equals("minecraft") || response.equals("java"))
+    {
+        openBookmarkFolder(4);
+    }
+    else if (response.equals("research") || response.equals("uni"))
+    {
+        openBookmarkFolder(3);
+    }
+
+    openApplication("intellij");
+    openApplication("github desktop");
+    githubAnalyzation();
+}
+
+/*
+ * Retrieve stored workflow state (store via enum)
+ */
+void setup()
+{
+    Serial.begin(9600);
+    // method testing code
+    delay(1000);
+    programmingWorkflow();
+}
+
+/*
+ * Check if on Linux: change boot order
+ * Check if on Windows: change boot order
+ * Restart machine
+ */
+void dualBootWorkflow()
+{
 }
 
 /*
@@ -106,15 +140,6 @@ void circuitsWorkflow()
  * Start backup processes
  */
 void systemAdminWorkflow()
-{
-}
-
-/*
- * Check for local commits that need to be pushed
- * Check origin for commit pulls
- * Check for code that has not been committed
- */
-void githubAnalyzation()
 {
 }
 
