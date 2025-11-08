0973f5f1b175614a8653fe7fecae3f4e2e721933
awesomebrownies <evan.n.brown25@gmail.com>
2025-11-07 17:02:54 -0700

    Add flash storage

--- src/hid-workflow-emulator.cpp
+++ src/hid-workflow-emulator.cpp
@@ -1,7 +1,38 @@
 #include <Arduino.h>
 #include <USBKeyboard.h>
+#include <kvstore_global_api.h>
+#include <LittleFileSystem.h>
+#include <FlashIAPBlockDevice.h>
+
+// Define flash block device (last 64KB of flash)
+static FlashIAPBlockDevice bd(XIP_BASE + (2 * 1024 * 1024 - 64 * 1024), 64 * 1024);
+static mbed::LittleFileSystem fs("fs", &bd);
 
 USBKeyboard key;
+
+/*
+ * Check if on Linux: change boot order
+ * Check if on Windows: change boot order
+ * Restart machine
+ */
+void dualBootWorkflow()
+{
+}
+
+/*
+ * Open platformIO folder
+ * Open KiCAD
+ */
+void circuitsWorkflow()
+{
+}
+
+/*
+ * Start backup processes
+ */
+void systemAdminWorkflow()
+{
+}
 
 /*
  * Check for local commits that need to be pushed
@@ -34,6 +65,7 @@ void openApplication(String s)
     delay(100);
     typeAnimation(s);
     key.key_code_raw(0x28); // enter
+    delay(100);
 }
 
 /*
@@ -91,10 +123,10 @@ String query(const char *prompt)
  */
 void programmingWorkflow()
 {
-    String response = query("Enter subworkflow? [minecraft], [research], [other] ... 5s");
+    String response = query("Enter subworkflow? [spigot], [research], [other] ... 5s");
 
     openApplication("firefox");
-    if (response.equals("minecraft") || response.equals("java"))
+    if (response.equals("spigot") || response.equals("java"))
     {
         openBookmarkFolder(4);
     }
@@ -108,39 +140,57 @@ void programmingWorkflow()
     githubAnalyzation();
 }
 
-/*
- * Retrieve stored workflow state (store via enum)
- */
 void setup()
 {
     Serial.begin(9600);
-    // method testing code
-    delay(1000);
-    programmingWorkflow();
-}
+    delay(1000); // wait for host to recognize USB device
 
-/*
- * Check if on Linux: change boot order
- * Check if on Windows: change boot order
- * Restart machine
- */
-void dualBootWorkflow()
-{
-}
+    int err = fs.mount(&bd);
+    if (err)
+    {
+        fs.reformat(&bd);
+    }
 
-/*
- * Open platformIO folder
- * Open KiCAD
- */
-void circuitsWorkflow()
-{
-}
+    const char *key = "workflowId";
+    int workflowId = 0;
+    kv_get(key, &workflowId, sizeof(workflowId), NULL);
 
-/*
- * Start backup processes
- */
-void systemAdminWorkflow()
-{
+    const int workflowMin = 0;
+    const int workflowMax = 3;
+
+    // if button is pressed, redefine workflowId using query()
+    if (digitalRead(12) == LOW) //!@#$%^&*()
+    {
+        // insert a * next to current workflowId
+        String prompt = "Select workflow ID: [0] Programming, [1] Dual Boot, [2] Circuits, [3] SysAdmin ... 5s";
+        prompt.replace("[" + String(workflowId) + "]", "[" + String(workflowId) + ":MEM-SEL]");
+        String response = query(prompt.c_str());
+        // Convert response to integer safely; String::toInt() does not throw
+        int testId = response.toInt();
+        if (testId >= workflowMin && testId <= workflowMax && testId != workflowId)
+        {
+            workflowId = testId;
+            kv_set(key, &workflowId, sizeof(workflowId), 0);
+        }
+    }
+
+    switch (workflowId)
+    {
+    case 0:
+        programmingWorkflow();
+        break;
+    case 1:
+        dualBootWorkflow();
+        break;
+    case 2:
+        circuitsWorkflow();
+        break;
+    case 3:
+        systemAdminWorkflow();
+        break;
+    default:
+        break;
+    }
 }
 
 void loop()
