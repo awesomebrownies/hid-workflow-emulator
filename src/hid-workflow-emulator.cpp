d803fc13d4486a2543972085212435f516567452
awesomebrownies <evan.n.brown25@gmail.com>
2025-11-10 21:13:51 -0700

    Add dual boot, circuits, & git analyze workflows

--- src/hid-workflow-emulator.cpp
+++ src/hid-workflow-emulator.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <USBKeyboard.h>
+#include <USBMouse.h>
 #include <kvstore_global_api.h>
 #include <LittleFileSystem.h>
 #include <FlashIAPBlockDevice.h>
@@ -9,39 +10,6 @@ static FlashIAPBlockDevice bd(XIP_BASE + (2 * 1024 * 1024 - 64 * 1024), 64 * 102
 static mbed::LittleFileSystem fs("fs", &bd);
 
 USBKeyboard key;
-
-/*
- * Check if on Linux: change boot order
- * Check if on Windows: change boot order
- * Restart machine
- */
-void dualBootWorkflow()
-{
-}
-
-/*
- * Open platformIO folder
- * Open KiCAD
- */
-void circuitsWorkflow()
-{
-}
-
-/*
- * Start backup processes
- */
-void systemAdminWorkflow()
-{
-}
-
-/*
- * Check for local commits that need to be pushed
- * Check origin for commit pulls
- * Check for code that has not been committed
- */
-void githubAnalyzation()
-{
-}
 
 /*
  * Loops through each character and types it
@@ -65,15 +33,71 @@ void openApplication(String s)
     delay(100);
     typeAnimation(s);
     key.key_code_raw(0x28); // enter
-    delay(100);
 }
 
 /*
- * Ctrl + B - Opens bookmark menu
+ * Check if on Linux: change boot order
+ * Check if on Windows: change boot order
+ * Restart machine
  */
-void openBookmarkFolder(int bookmarkItem)
+void dualBootWorkflow()
 {
-    // do this later!
+    const char *kv_key = "bootOrder";
+    int bootOrder = 0; // 0001: Linux, 0000: Windows
+    kv_get(kv_key, &bootOrder, sizeof(bootOrder), NULL);
+    if (bootOrder == 1)
+    {
+        // windows sequence, no need to set next temporary next boot
+        int newBootOrder = 0;
+        kv_set(kv_key, &newBootOrder, sizeof(newBootOrder), 0); // set next boot to Linux
+
+        key.key_code('r', 0x08); // windows+r
+        delay(300);
+        key.printf("shutdown /r /t 0\n");
+    }
+    else
+    {
+        // linux sequence
+        int newBootOrder = 1;
+        kv_set(kv_key, &newBootOrder, sizeof(newBootOrder), 0);
+
+        key.key_code('t', 0x05); // terminal shortcut
+        delay(750);
+        key.printf("bash -c 'sudo efibootmgr -n 000"); // example for Linux to boot Windows next
+        key.printf("%d", bootOrder);
+        key.printf(";reboot'\n");
+    }
+}
+
+/*
+ * Open platformIO folder
+ * Open KiCAD
+ */
+void circuitsWorkflow()
+{
+    openApplication("vscode");
+    delay(500);
+    openApplication("kicad");
+}
+
+/*
+ * Start backup processes
+ */
+void systemAdminWorkflow()
+{
+}
+
+/*
+ * Check for local commits that need to be pushed
+ * Check origin for commit pulls
+ * Check for code that has not been committed
+ */
+void githubAnalyzation()
+{
+    key.key_code('t', 0x05); // terminal shortcut
+    delay(750);
+    key.printf("cd ./Documents/GitHub\n");
+    key.printf("mgitstatus\n");
 }
 
 String query(const char *prompt)
@@ -86,7 +110,7 @@ String query(const char *prompt)
 
     delay(1000);
 
-    String response = "";
+    String response = "-1";
     unsigned long timeout = millis() + 5000;
 
     while (millis() < timeout)
@@ -123,20 +147,15 @@ String query(const char *prompt)
  */
 void programmingWorkflow()
 {
+    // no subworkflows at the moment, can be defined later
     String response = query("Enter subworkflow? [spigot], [research], [other] ... 5s");
 
     openApplication("firefox");
-    if (response.equals("spigot") || response.equals("java"))
-    {
-        openBookmarkFolder(4);
-    }
-    else if (response.equals("research") || response.equals("uni"))
-    {
-        openBookmarkFolder(3);
-    }
-
+    delay(500);
     openApplication("intellij");
+    delay(500);
     openApplication("github desktop");
+    delay(2000);
     githubAnalyzation();
 }
 
@@ -159,10 +178,10 @@ void setup()
     const int workflowMax = 3;
 
     // if button is pressed, redefine workflowId using query()
-    if (digitalRead(12) == LOW) //!@#$%^&*()
+    if (digitalRead(12) == LOW)
     {
         // insert a * next to current workflowId
-        String prompt = "Select workflow ID: [0] Programming, [1] Dual Boot, [2] Circuits, [3] SysAdmin ... 5s";
+        String prompt = "Select ID: [0] Programming, [1] Dual Boot, [2] Circuits, [3] Sys-Admin";
         prompt.replace("[" + String(workflowId) + "]", "[" + String(workflowId) + ":MEM-SEL]");
         String response = query(prompt.c_str());
         // Convert response to integer safely; String::toInt() does not throw
